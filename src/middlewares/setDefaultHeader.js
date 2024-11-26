4051e6ec698679aaff38e90f4a0cb8ec9e239db1
Alvantesha Priliandi <alvanteshapriliandi@Alvanteshas-MacBook-Air.local>
2024-11-26 13:54:54 +0700

    added global header

--- src/middlewares/setDefaultHeader.js
+++ src/middlewares/setDefaultHeader.js
@@ -1,5 +1,8 @@
 function setDefaultHeader(req, res, next) {
     res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next()
 }
 
